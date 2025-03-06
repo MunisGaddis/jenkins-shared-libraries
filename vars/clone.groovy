dac1caf91024426241245a985054dff8fb980e4d
MunisGaddis <munishkapoorin@outlook.com>
2025-03-06 10:18:17 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,1 +1,6 @@
+def call(String url, String branch){
+  echo 'This is cloning code'
+  git url: "https://github.com/LondheShubham153/django-notes-app.git", branch:"main"
+  echo "Code Cloning successfully"
+}
 
